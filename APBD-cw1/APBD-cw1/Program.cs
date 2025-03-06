3b1029be8ccdf54fff28dbcd8fe7b4e7c4001b94
unknown <s30749@pjwstk.edu.pl>
2025-03-06 19:25:59 +0100

    feature-average

--- APBD-cw1/APBD-cw1/Program.cs
+++ APBD-cw1/APBD-cw1/Program.cs
@@ -3,3 +3,8 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Modyfikacja numero uno");
 Console.WriteLine("Modyfikacja numero dos");
 Console.WriteLine("Modyfikacja numero tres");
+
+static double ObliczSrednia(int[] liczby)
+{
+    return liczby.Length == 0 ? 0 : liczby.Average();
+}
